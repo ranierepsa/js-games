55489fb9f873f78aef26730492ac6565cf3ade73
ranierearaujo <ranierepsa@gmail.com>
2023-11-07 15:46:47 -0300

    feat: Adding credits, sound logic, gameover logic and matching cards logic

--- src/scripts/Card.js
+++ src/scripts/Card.js
@@ -2,34 +2,39 @@ export default class Card {
     isFlipped = false;
     isMatched = false;
 
-    constructor (gridPosition, imageId, imagePath, imageView) {
+    constructor (gridPosition, imageId, imagePath, divView) {
         this.gridPosition = gridPosition;
         this.imageId = imageId;
         this.imagePath = imagePath;
-        this.imageView = imageView;
+        this.divView = divView;
     }
 
     canClick() {
         return !this.isFlipped;
     }
 
+    showImage() {
+        this.divView.childNodes[0].src = this.imagePath;
+        this.divView.classList.remove('div-item-hidden-background');
+    }
+
+    hideImage() {
+        this.divView.childNodes[0].src = '';
+        this.divView.classList.add('div-item-hidden-background');
+        this.isFlipped = false;
+    }
+
     flip() {
-        if (this.canClick()) {
-            this.isFlipped;
-        }
+        this.isFlipped = this.canClick();
+        !this.canClick() ? this.showImage() : this.hideImage();
     }
 
     match(card) {
-        if (isMath(card)) {
-            this.isMatch = true;
-            card.isMatch = true;
-        } else {
-            this.isFlipped = false;
-            card.isFlipped = false;
-        }
+        this.isMatch = isMath(card);
+        card.isMatch = isMath(card);
     }
 
     isMatch(card) {
-        return card.canClick() && card.imageId === this.imageId;
+        return card.imageId === this.imageId;
     }
 }
--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -6,35 +6,66 @@ const imagePaths = [
     {id: 3, src: './src/images/animals/giraffe.png'},
     {id: 4, src: './src/images/animals/elephant.png'},
     {id: 5, src: './src/images/animals/cow.png'},
-    {id: 6, src: './src/images/animals/panda.png'}
+    {id: 6, src: './src/images/animals/panda.png'},
+    {id: 7, src: './src/images/animals/penguin.png'},
+    {id: 8, src: './src/images/animals/pig.png'}
 ];
 
 const game = {
     view: document.getElementsByClassName('game')[0],
-    cards: []
+    flippedCard: null,
+    isInputLocked: false,
+    remainingMatches: imagePaths.length
 }
 
-function onClickCard(elem) {
-    const card = elem.target.card;
-
-    console.log(card.imagePath);
-
-    // Exibindo a imagem da carta.
-    elem.target.childNodes[0].src = card.imagePath;
-    console.log(elem.target);
-
-    // Escondendo o backgroud da carta.
-    elem.target.classList.remove('div-item-hidden-background');
-
+function checkGameOver() {
+    if (game.remainingMatches <= 0) {
+        new Audio('./src/audios/win.mp3').play();
+    }
 }
 
-// function createCardView(cardNumber) {
-//     const cardView = document.createElement('div');
-//     cardView.id = cardNumber;
-//     cardView.addEventListener('click', onClickCard, this);
-//     cardView.classList.add('item');
-//     return cardView;
-// }
+function decreaseRemainingMatches() {
+    game.remainingMatches--;
+    checkGameOver();
+}
+
+function disableBothCardsClickEvent(card1, card2) {
+    card1.divView.removeEventListener('click', flipCard);
+    card2.divView.removeEventListener('click', flipCard);
+}
+
+function noMatchEvent(card1, card2) {
+    card1.hideImage();
+    card2.hideImage();
+    game.isInputLocked = false;
+}
+
+function flipCard(event) {
+    if (game.isInputLocked) return;
+
+    const htmlElem = event.target;
+    const card = htmlElem.card ? htmlElem.card : htmlElem.parentElement.card;
+
+    card.flip();
+
+    if (!game.flippedCard) {
+        game.flippedCard = card;
+        new Audio('./src/audios/click.mp3').play();
+    } else {
+        if (card.isMatch(game.flippedCard)) {
+            card.isMatched = true;
+            game.flippedCard.isMatched = true;
+            disableBothCardsClickEvent(card, game.flippedCard);
+            decreaseRemainingMatches();
+            new Audio('./src/audios/hit.wav').play();
+        } else {
+            new Audio('./src/audios/miss.wav').play();
+            game.isInputLocked = true;
+            setTimeout(noMatchEvent, 1000, card, game.flippedCard);
+        }
+        game.flippedCard = null;
+    }
+}
 
 function randomizeArrayWithFisherYatesAlgorithm(array) {
     var count = array.length, randomNumber, temp;
@@ -54,13 +85,13 @@ function generateCards() {
         const img = document.createElement('img');
         img.id = index;
         // img.src = value.src;
-        div.addEventListener('click', onClickCard);
+        div.addEventListener('click', flipCard);
         img.classList.add('img-item');
         div.classList.add('div-item-hidden-background');
         div.classList.add('div-item');
         div.appendChild(img);
         game.view.appendChild(div);
-        const card = new Card(index, value.id, value.src, img);
+        const card = new Card(index, value.id, value.src, div);
         div.card = card;
         return card;
     });
